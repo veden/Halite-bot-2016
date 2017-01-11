f1dd8f1d426d83222c2c90d561fc17092bfe0e70
Aaron Veden <veden@ivcore.com>
2017-01-10 20:09:01 -0800

    fixed

--- logic/util/MathUtil.java
+++ logic/util/MathUtil.java
@@ -4,7 +4,11 @@ public class MathUtil {
 
     public static float normalize(float x, float low, float high) {
 	if (low == high)
-	    return 1;
+	    return 1f;
+	if (x < low)
+	    return 0;
+	if (x > high)
+	    return 1f;
 	return ((x - low) / (high - low));
     }
 }
