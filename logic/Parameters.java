04f4917576cf8d47e1209563ebb1c616fef5a248
Aaron Veden <veden@ivcore.com>
2017-01-26 23:14:14 -0800

    354.35

--- logic/Parameters.java
+++ logic/Parameters.java
@@ -14,7 +14,7 @@ public class Parameters {
     public static float objectiveThreshold = 0.55f;
     public static float objectiveUnitSpread = 0.7f;
     public static float objectiveGeneratorSpread = 0.25f;
-    public static float reinforceSpread = 0.10f;
+    public static float reinforceSpread = 0.09f;
     public static float enemyGeneratorWeight = 0.4f;
     public static float enemyUnitWeight = 0.1f;
     public static float enemyGeneratorSpread = 0.7f;
