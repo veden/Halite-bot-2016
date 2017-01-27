ef2b3a66220885a7c2e8283b35171be52333b384
Aaron Veden <veden@ivcore.com>
2017-01-26 19:10:10 -0800

    sort for debugger

--- game/Stats.java
+++ game/Stats.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
@@ -31,7 +34,14 @@ public class Stats {
     
     public static String siteCounts() {
 	StringBuilder sb = new StringBuilder();
-	for (Entry<Float, Float> count : siteCounter.entrySet()) {
+	ArrayList<Entry<Float, Float>> counts = new ArrayList<Entry<Float, Float>>();
+	counts.addAll(siteCounter.entrySet());
+	Collections.sort(counts, new Comparator<Entry<Float, Float>>() {
+		public int compare(Entry<Float, Float> e1, Entry<Float, Float> e2) {
+		    return (e1.getKey() - e2.getKey()) > 0 ? 1 : -1;
+		}
+	    });
+	for (Entry<Float, Float> count : counts) {
 	    if (sb.length() > 0)
 		sb.append(" ");
 	    sb.append(count.getKey() + " " + count.getValue());
